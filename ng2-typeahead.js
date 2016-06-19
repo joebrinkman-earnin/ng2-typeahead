d91c4a1f8cdc24f12ecdf6580f969d8cc24a6aed
j583306 <joseph.brinkman@8451.com>
2016-06-19 00:36:27 -0400

    removing demo

--- ng2-typeahead.js
+++ ng2-typeahead.js
@@ -1,5 +1,5 @@
-'use strict';
+"use strict";
 function __export(m) {
-  for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 __export(require('./components/typeahead/ng2-typeahead'));
